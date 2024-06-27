52e42ca9e51c3d6bc050f3494e01adb679b1e70b
Bartek <bartlomiej.kacki@gmail.com>
2024-06-27 17:53:37 +0200

    Add project files.

---
+++ CardEnum/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CardEnum
+{
+    internal class Program
+    {
+        private static readonly Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            Card randomCard = new Card((Values)random.Next(1, 14), (Suits)random.Next(4));
+            Console.WriteLine(randomCard.Name.ToString());
+        }
+    }
+}
